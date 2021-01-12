4ca3b5f7619edc51446572627454aaa09d49071d
sziccardi1998 <sziccardifullstack@gmail.com>
2021-01-12 14:18:36 -0500

    Addition of basic inquirer prompt.

--- server.js
+++ server.js
@@ -13,4 +13,19 @@ connection.connect(err => {
     if (err) throw err;
     console.log("Conencted as id" + connection.threadId);
     // insert function that starts the cli
-})
+});
+
+function optionTree() {
+    inquirer.prompt([{
+        type: 'list',
+        message: 'What would you like to do?',
+        name: 'selection',
+        choices: ['View all employees', 'View department', 'View role', 'Add department', 'Add role', 'Add employee'],
+    }]).then((data) => {
+        const userChoice = data.selection;
+        // build a case-switch tree that performs a different task for each choice
+        case(userChoice) {
+
+        }
+    })
+}
