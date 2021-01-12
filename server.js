ecd673dfb7dfa4de402bdc850c79c1d6fc191481
sziccardi1998 <sziccardifullstack@gmail.com>
2021-01-12 13:25:24 -0500

    server connection created.

--- server.js
+++ server.js
@@ -1,2 +1,16 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: // figure out how to hide this later
+    database: //whatever we name this thing later
+});
+
+connection.connect(err => {
+    if (err) throw err;
+    console.log("Conencted as id" + connection.threadId);
+    // insert function that starts the cli
+})
