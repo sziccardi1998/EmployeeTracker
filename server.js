ee60e4defb25088d7bc5018009910b59e9d099b2
sziccardi1998 <sziccardifullstack@gmail.com>
2021-01-13 15:45:01 -0500

    addition of function skeletons.

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 
+// import other functions
+const addDepartment = require('./lib/addDepartment');
+const addEmployee = require('./lib/addEmployee');
+
 const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
-    password: // figure out how to hide this later
-    database: //whatever we name this thing later
+    password: "", // figure out how to hide this later
+    database: "employeeTracker_DB" //whatever we name this thing later
 });
 
 connection.connect(err => {
     if (err) throw err;
     console.log("Conencted as id" + connection.threadId);
     // insert function that starts the cli
+    optionTree();
 });
 
 function optionTree() {
@@ -22,7 +27,7 @@ function optionTree() {
         name: 'selection',
         choices: ['View all employees', 'View department', 'View role', 'Add department', 'Add role', 'Add employee', 'Exit'],
     }]).then((data) => {
-        const userChoice = data.selection;
+        let userChoice = data.selection;
         // build a case-switch tree that performs a different task for each choice
         // each case should have a function that is used
         switch (userChoice) {
@@ -33,14 +38,15 @@ function optionTree() {
             case 'View role':
                 break;
             case 'Add department':
+                addDepartment();
                 break;
             case 'Add role':
                 break;
             case 'Add employee':
+                addEmployee();
                 break;
             case 'Exit':
                 break;
         }
     })
 }
-
